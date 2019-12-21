458c246c9e30a9bdd683aba1c0c931540daa9a04
Pedro A. Aranda Gutiérrez <paaguti@gmail.com>
2019-12-21 06:46:42 +0100

    Handle localhost for IPv4 and IPv6

--- common/netfuncs.go
+++ common/netfuncs.go
@@ -45,29 +45,36 @@ func UdpFamily(ip string) (string, error) {
 }
 
 func FirstIP(host string, ipv6 bool) (string, error) {
-
-	if ipv6 {
-		if host == "localhost" {
-			host = "ip6-" + host
+	//
+	// Some systems name localhost differently for IPv6
+	// Always use ::1 or 127.0.0.1 for localhost
+	//
+	if host == "localhost" {
+		if ipv6 {
+			return "::1", nil
+		} else {
+			return "127.0.0.1", nil
 		}
+	}
+	family := "IPv4"
+	if ipv6 {
+		family = "IPv6"
 	}
 	ips, err := net.LookupIP(host)
 	if err == nil {
 		for _, ip := range ips {
 			if ip.To4() == nil {
-				if ipv6 {
-					return ip.String(), nil
+				if !ipv6 {
+					continue
 				}
 			} else {
-				if ipv6 == false {
-					return ip.String(), nil
+				if ipv6 {
+					continue
 				}
 			}
+			return ip.String(), nil
 		}
-		if ipv6 {
-			return "", errors.New(fmt.Sprintf("Couldn't find IPv6 address for %s\n", host))
-		}
-		return "", errors.New(fmt.Sprintf("Couldn't find IPv4 address for %s\n", host))
+		return "", errors.New(fmt.Sprintf("Couldn't find %s address for %s\n", family, host))
 	}
 	return "", err
 }
